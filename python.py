3ca996f6dc39b7094e8d86c724bc11cbe5b4df63
Dishone Prabu <dishoneprabu959@gmail.com>
2023-07-20 23:15:01 +0530

    On branch main

--- python.py
+++ python.py
@@ -1,12 +1,16 @@
-def add_numbers(a, b):
-    """
-    This function adds two numbers.
+# Uncomment the below two lines for taking the User Inputs
+#start = int(input("Enter the start of range: "))
+#end = int(input("Enter the end of range: "))
 
-    :param a: The first number.
-    :type a: int
-    :param b: The second number.
-    :type b: int
-    :return: The sum of the two numbers.
-    :rtype: int
-    """
-    return a + b
+# Range declaration
+start = 5
+end = 20
+
+if start % 2 != 0:
+
+	for num in range(start, end + 1, 2):
+		print(num, end=" ")
+else:
+
+	for num in range(start+1, end + 1, 2):
+		print(num, end=" ")
