dd83ac0aae911bfda9392fc8aec3aa6beee40b04
Dishone Prabu <dishoneprabu959@gmail.com>
2023-07-20 21:59:49 +0530

    On branch main

--- bash.sh
+++ bash.sh
@@ -2,13 +2,6 @@
 
 pip install sphinx
 
-# Check if Sphinx is installed
-if ! command -v sphinx-quickstart &> /dev/null
-then
-    echo "Sphinx is not installed. Please install it using 'pip install sphinx'."
-    exit 1
-fi
-
 # Prompt the user for project details
 read -p "Enter the project name: " project_name
 read -p "Enter the author name: " author_name
