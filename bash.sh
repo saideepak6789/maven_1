429eeedc7a1f512c40e1bb90146a521b2345a0e9
Dishone Prabu <dishoneprabu959@gmail.com>
2023-07-20 21:56:45 +0530

    On branch main

--- bash.sh
+++ bash.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
-sphinx-quickstart
-cp _build/html/index.html ./index.html
+# Check if Sphinx is installed
+if ! command -v sphinx-quickstart &> /dev/null
+then
+    echo "Sphinx is not installed. Please install it using 'pip install sphinx'."
+    exit 1
+fi
+
+# Prompt the user for project details
+read -p "Enter the project name: " project_name
+read -p "Enter the author name: " author_name
+read -p "Enter the version number: " version_number
+
+# Run sphinx-quickstart with provided project details
+sphinx-quickstart \
+    --project="$project_name" \
+    --author="$author_name" \
+    --version="$version_number"
+    
+cp ./_build/html/index.html ./index.html
