26ecd87d4312125fd033b94c301c296daa4d3410
Dishone Prabu <dishoneprabu959@gmail.com>
2023-07-20 22:23:32 +0530

    On branch main

--- bash.sh
+++ bash.sh
@@ -6,8 +6,8 @@ project_name=$1
 author_name=$2
 version_number=$3
 
-
+python -m sphinx.cmd.quickstart
 # Run sphinx-quickstart with provided project details
-sphinx-quickstart -q -p $project_name -a $author_name -v $version_number -l en 
+# -q -p $project_name -a $author_name -v $version_number -l en 
 
 cp ./_build/html/index.html ./index.html
