220af8b9ff35de8156fa53a77787b6679401ef90
Dishone Prabu <dishoneprabu959@gmail.com>
2023-07-23 21:51:31 +0530

    On branch main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello Dishone! Your project has build successfully✌️";
+    return "Hello Jenitha!";
   }
 }
