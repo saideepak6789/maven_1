1f56ead71b7de66f8d89921721b2c48413a10e11
Dishone Prabu <dishoneprabu959@gmail.com>
2023-07-24 10:02:33 +0530

    On branch main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,29 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "<!DOCTYPE html>\n" +
-	"<html>\n" +
-	"<head>\n" +
-	"    <title>Welcome to My Website</title>\n" +
-	"    <style>\n" +
-	"        body {\n" +
-	"            background-image: url('image.jpg');\n" +
-	"            background-size: cover;\n" +
-	"            background-repeat: no-repeat;\n" +
-	"            background-position: center;\n" +
-	"            height: 100vh;\n" +
-	"            display: flex;\n" +
-	"            justify-content: center;\n" +
-	"            align-items: center;\n" +
-	"            font-family: Arial, sans-serif;\n" +
-	"            color: white;\n" +
-	"            font-size: 24px;\n" +
-	"        }\n" +
-	"    </style>\n" +
-	"</head>\n" +
-	"<body>\n" +
-	"    <h1>Hello Dishone! Your project has built successfully✌️</h1>\n" +
-	"</body>\n" +
-	"</html>";
+    return "Hello Dishone! Your project has built successfully✌️";
   }
 }
